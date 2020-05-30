eac3e1e892d49caf3012fee64f843cc661541e3c
Xiaoxuan Wang <wangxiaoxuan4869@gmail.com>
2020-05-29 20:09:50 -0700

    Add payment model and forms for cards

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_202641) do
+ActiveRecord::Schema.define(version: 2020_05_29_210642) do
+
+  create_table "payments", force: :cascade do |t|
+    t.string "email"
+    t.string "token"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
